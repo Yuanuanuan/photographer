cc01ee59231fbd43da9fbebf6e4825894dfa4f62
Yuanuanuan <asdll6618@gmail.com>
2023-09-24 22:19:21 +0800

    feat: test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,18 +8,15 @@ const Homepage = () => {
   let [data, setData] = useState(null);
   let [page, setPage] = useState(1);
   let [currentSearch, setCurrentSearch] = useState("")
+  let [loadingMore, setLoadingMore] = useState(false);
   const auth = "qxWbhXu3WCyLDTgXfBDPFPHHPHXxdKqnvn6E5MiGTKTyFLrMKH5lTPcf";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
-  const search = async(url) => {
-    let result = await axios.get(url, { headers: { Authorization: auth },
-    });
-    setData(result.data.photos);
-    setCurrentSearch(input);
-  }
-
-  const morePicture = async() => {
+  
+  const morePicture = async () => {
+    if (loadingMore) return;
+    setLoadingMore(true);
     let newURL;
     setPage(page + 1)
     if (currentSearch === "") {
@@ -27,37 +24,43 @@ const Homepage = () => {
     } else {
       newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`
     }
-    let result = await axios.get(newURL, { headers: { Authorization: auth },
-    });
+    let result = await axios.get(newURL, { headers: { Authorization: auth } });
     setData(data.concat(result.data.photos));
+    setLoadingMore(false);
+  }
+
+  const search = async (url) => {
+    let result = await axios.get(url, { headers: { Authorization: auth } });
+    setData(result.data.photos);
+    setCurrentSearch(input);
   }
 
   useEffect(() => {
     search(initialURL);
-  }, []);
+  }, [])
 
   useEffect(() => {
     if (input === '') {
       search(initialURL);
     }
-  }, [input])
+  }, [input]);
 
   return (
-  <div style={{ minHeight: "100vh" }}>
-    <Search 
-    search={() => {search(searchURL);}} 
-    input={input}
-    setInput={setInput} 
-    />
-    <div className='pictures'>
-      {data && data.map((d, index) => {
-        return <Picture key={index} data={d} />;
-      })}
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => { search(searchURL) }}
+        input={input}
+        setInput={setInput}
+      />
+      <div className='pictures'>
+        {data && data.map((d, index) => {
+          return <Picture key={index} data={d} />;
+        })}
+      </div>
+      <div className='morePicture'>
+        <button onClick={morePicture} disabled={loadingMore}>Load More</button>
+      </div>
     </div>
-    <div className='morePicture'>
-      <button onClick={morePicture}>Leran More</button>
-    </div>
-  </div>
   )
 }
 
